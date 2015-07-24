29c4106fdb632826a0ce003f623ed7d1575da0ff
Jake Seeber <zippyjake1@gmail.com>
2015-07-24 09:04:43 -0400

    save

---
+++ shooter/game.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var game = new Phaser.Game(800,600,Phaser.AUTO,'game',
+  {preload:preload,create:create,update:update,render:render});
+
+function preload() {
+  game.stage.backgroundColor = '55FF00';
+}
+
+function create() {
+}
+
+function update() {
+}
+
+function render() {
+}
