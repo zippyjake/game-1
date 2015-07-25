85a216237258d749731fb250233f305d9a0dc3c9
Jake Seeber <zippyjake1@gmail.com>
2015-07-25 11:53:17 -0400

    save

--- shooter/game.js
+++ shooter/game.js
@@ -5,12 +5,27 @@ var game = new Phaser.Game(800,600,Phaser.AUTO,'game',
 
 function preload() {
   game.stage.backgroundColor = '55FF00';
+  game.load.image('ship', 'ship.gif');
 }
 
+var ship;
+var cursors;
+
 function create() {
+  ship = game.add.sprite(20,300,'ship');
+  game.physics.enable(ship, Phaser.Physics.ARCADE);
+  cursors = game.input.keyboard.createCursorKeys();
 }
 
 function update() {
+  ship.body.velocity.x = 0;
+  ship.body.velocity.y = 0;
+  if (cursors.up.isDown) {
+    ship.body.velocity.y = -300;
+  }
+  else if (cursors.down.isDown) {
+    ship.body.velocity.y = 300;
+  }
 }
 
 function render() {
